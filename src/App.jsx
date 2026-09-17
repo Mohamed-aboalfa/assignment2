759a37179c105c68ed4dd2f8cda2c9ded076182b
aboalfa <aboalfam1@gmail.com>
2026-09-17 06:12:41 +0300

    update project

--- src/App.jsx
+++ src/App.jsx
@@ -1,47 +1,58 @@
-import { useState } from 'react'
-import "./App.css"
-import Button from './Button'
-import Card from './Card'
+import { useState } from "react";
+import "./App.css";
+import Button from "./Button";
+import Card from "./Card";
 
 import logo1 from "./assets/react.svg";
 import logo2 from "./assets/vite.svg";
-
-
-
-
+import Table from "./Table";
 
 function App() {
-  
- const [style,setStyle] =useState("ml-8 mt-4") 
-  function click(){
-       setStyle((e)=>{
-       return e==="ml-8 mt-4"?"bg-blue-500 ml-4 mt-4 rounded text-white w-16 h-8":"ml-8 mt-4"
-       })
-    
+  const [style, setStyle] = useState("ml-8 mt-4");
+  function click() {
+    setStyle((e) => {
+      return e === "ml-8 mt-4"
+        ? "bg-blue-500 ml-4 mt-4 rounded text-white w-16 h-8"
+        : "ml-8 mt-4";
+    });
   }
-  const [styling,setStyling] =useState("ml-8 mt-4") 
-  function onClick(){
-       setStyling((e)=>{
-       return e==="ml-8 mt-4"?"bg-green-500 ml-4 mt-4 rounded text-white w-16 h-8":"ml-8 mt-4"
-       })
-    
+  const [styling, setStyling] = useState("ml-8 mt-4");
+  function onClick() {
+    setStyling((e) => {
+      return e === "ml-8 mt-4"
+        ? "bg-green-500 ml-4 mt-4 rounded text-white w-16 h-8"
+        : "ml-8 mt-4";
+    });
   }
-
+  const data = {
+    coulmnsName: { col1: "name", col2: "age", col3: "city" },
+    dataName: { name: "mohamed", age: "20", city: "tanta" },
+  };
 
   return (
     <>
-     <Button text={"click"} onClick={click}   style={style}/>
-     <Button text={"click me"} onClick={onClick} style={styling}/>
-     <Card title={"aboalfa1"} description={"i'm a frontend react developer"} img={logo1} alt={"react photo"} >
-      hello world
-     </Card>
-     <hr></hr>
-      <Card title={"aboalfa2"} description={"i'm a mern fullstack developer"} img={logo2} alt={"vite photo"} >
-     not hello world 😉😎
-     </Card>
-    
+      <Button text={"click"} onClick={click} style={style} />
+      <Button text={"click me"} onClick={onClick} style={styling} />
+      <Card
+        title={"aboalfa1"}
+        description={"i'm a frontend react developer"}
+        img={logo1}
+        alt={"react photo"}
+      >
+        hello world
+      </Card>
+      <hr></hr>
+      <Card
+        title={"aboalfa2"}
+        description={"i'm a mern fullstack developer"}
+        img={logo2}
+        alt={"vite photo"}
+      >
+        not hello world 😉😎
+      </Card>
+      <Table data={data}/>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/Table.jsx
@@ -0,0 +1,32 @@
+
+
+function Table  ({data})  {
+  return (
+    <div>
+      <table>
+  <thead>
+    <tr>
+<th>{data.coulmnsName.col1}</th>
+<th>{data.coulmnsName.col2}</th>
+<th>{data.coulmnsName.col3}</th>
+    </tr>
+  </thead>
+
+  <tbody>
+    <tr className="gap-4">
+      <td>{data.dataName.name}</td>
+      <td>{data.dataName.age}</td>
+      <td>{data.dataName.city}</td>
+    </tr>
+
+    
+   
+  </tbody>
+
+ 
+</table>
+    </div>
+  )
+}
+
+export default Table
